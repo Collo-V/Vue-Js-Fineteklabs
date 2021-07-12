993e43e6d86c92d249213fc1336bcdde84de2676
Colo-V <collinsvictoromondi@gmail.com>
2021-07-12 15:06:21 +0300

    First

---
+++ facebook/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Login msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+#app {
+  height: 700px;position: relative;
+  
+  
+}
+body{position: relative;;}
+</style>
---
+++ school/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+<h1>{{msg}}</h1>
+<p>So here are, diving into vue js3</p>
+
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+   return{
+     msg:"Welcome "
+   }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+p{font-size: 18px;font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;}
+</style>
